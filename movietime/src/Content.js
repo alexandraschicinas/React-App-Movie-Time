d7ae34dea956b8f856f514d9c81c8d1ca37f07b5
Adelinanica <69160568+Adelinanica@users.noreply.github.com>
2020-10-12 14:33:40 +0300

    add

---
+++ movietime/src/Content.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+    const Content = ()=> {
+        return (
+            <section>
+                
+            <div  className="content">
+                <div id="content1">
+                <h1> Trending Movie</h1>
+                </div>
+
+                <div id="content2">
+                <h1>Movie List</h1>
+                </div>
+            </div>
+            
+            </section>
+        )
+    }
+
+export default Content;
