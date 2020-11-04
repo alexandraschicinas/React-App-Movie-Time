46588920faf05e442edf51e10840a58f23772122
Adelinanica <69160568+Adelinanica@users.noreply.github.com>
2020-11-04 20:37:19 +0200

    add

--- movietime/src/App.js
+++ movietime/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch } from 'react-router-dom'
- import Home from './Home';
-import NavBar from './NavBar';
- import MyList from './MyList';
-import MovieType from './MovieType'
-import './index.css' 
-
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import MyList from "./MyList";
+import MovieType from "./MovieType";
+import "./index.css";
 
 const App = () => {
   return (
-    <BrowserRouter>   
-          <NavBar />
-    <Switch>
-      <Route exact={true} path='/' component={Home} />
-      <Route exact path="/movie/:id" component={MovieType}/>
-      <Route  exact path="/Mylist" component={MyList}/>
-    </Switch>
-    </BrowserRouter> 
-   );
-}
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact={true} path="/" component={Home} />
+        <Route exact path="/movie/:id" component={MovieType} />
+        <Route exact path="/Mylist" component={MyList} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
